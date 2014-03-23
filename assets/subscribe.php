3c1c3fc48b91f2704729bf39ebe6da39a07ebf6a
Victor Xian Wang <victor.xianwang@gmail.com>
2014-03-23 21:59:53 +0800

    Coming soon almost done.

--- assets/subscribe.php
+++ assets/subscribe.php
@@ -10,7 +10,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the notification when someone subscribes
-    $emailTo = 'contact.azmind@gmail.com';
+    $emailTo = 'info@umunc.com';
 
     $subscriber_email = addslashes(trim($_POST['email']));
 
@@ -27,11 +27,11 @@ if($_POST) {
         echo json_encode($array);
 
         // Send email
-	    $subject = 'New Subscriber (mira)!';
-	    $body = "You have a new subscriber!\n\nEmail: " . $subscriber_email;
+	    $subject = 'New Subscriber on UMUNC.COM';
+	    $body = "There's a new subscriber on UMUNC.COM!\n\nEmail: " . $subscriber_email;
         // uncomment this to set the From and Reply-To emails, then pass the $headers variable to the "mail" function below
 	    //$headers = "From: ".$subscriber_email." <" . $subscriber_email . ">" . "\r\n" . "Reply-To: " . $subscriber_email;
-	    mail($emailTo, $subject, $body);
+	    mail($emailTo, $subject, $body, "From: info@umunc.com");
     }
 
 }
